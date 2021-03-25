6570179ac5c64cedff41c4ef626e3e7794fb6e91
akmedof <aslanahmedof@gmail.com>
2021-03-25 13:14:20 -0700

    NewsRowBinding create onNewsClickListener functions

--- app/src/main/java/com/texnodevcom/texnodev/binding/NewsRowBinding.kt
+++ app/src/main/java/com/texnodevcom/texnodev/binding/NewsRowBinding.kt
@@ -1,13 +1,31 @@
 package com.texnodevcom.texnodev.binding
 
+import android.util.Log
+import android.widget.LinearLayout
+import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
+import com.texnodevcom.texnodev.view.NewsFragmentDirections
+
 class NewsRowBinding {
 
-//    companion object{
-//
-//
-//        fun download
-//
-//    }
+    companion object {
+
+        @BindingAdapter("onNewsClickListener")
+        @JvmStatic
+        fun onNewsClickListener(newsRowLayout: LinearLayout, id: Int) {
+            Log.d("onNewsClickListener", "CALLED")
+            newsRowLayout.setOnClickListener {
+                try {
+                    val action = NewsFragmentDirections.actionNewsFragmentToDetailsActivity(id)
+                    newsRowLayout.findNavController().navigate(action)
+                }catch (e: Exception){
+                    Log.d("onNewsClickListener", e.toString())
+                }
+            }
+
+        }
+
+    }
 
 
 }
--- app/src/main/java/com/texnodevcom/texnodev/view/DetailsActivity.kt
+++ app/src/main/java/com/texnodevcom/texnodev/view/DetailsActivity.kt
@@ -2,11 +2,26 @@ package com.texnodevcom.texnodev.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
 import com.texnodevcom.texnodev.R
+import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        setSupportActionBar(toolBar)
+        toolBar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
