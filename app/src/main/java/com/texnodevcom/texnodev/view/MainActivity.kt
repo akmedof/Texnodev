111087f9ba5f907ebd9ae38dbe10daf73e1bee2c
akmedof <aslanahmedof@gmail.com>
2021-03-25 14:01:57 -0700

    add BottomNavigationView edit: nav_graph.xml, ActivityMain

--- app/src/main/java/com/texnodevcom/texnodev/view/MainActivity.kt
+++ app/src/main/java/com/texnodevcom/texnodev/view/MainActivity.kt
@@ -2,11 +2,34 @@ package com.texnodevcom.texnodev.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.texnodevcom.texnodev.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment)
+        val appBarConfiguration : AppBarConfiguration = AppBarConfiguration(setOf(
+            R.id.newsFragment,
+            R.id.favoriteFragment
+        ))
+
+        bottomBar.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
