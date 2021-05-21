4ace73c397072c7e64b07bdbeee9381a59ca43b9
akmedof <aslanahmedof@gmail.com>
2021-05-21 10:57:29 +0400

    Upadate: Data Binding, Row Layout, DateTime Row

---
+++ app/src/main/java/com/texnodevcom/texnodev/model/Category.kt
@@ -0,0 +1,13 @@
+package com.texnodevcom.texnodev.model
+
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import com.google.gson.annotations.SerializedName
+
+//@Entity
+//data class Category(
+//    @ColumnInfo(name = "name")
+//    @SerializedName("name")
+//    val name: String
+//)
---
+++ app/src/main/java/com/texnodevcom/texnodev/model/CategoryNews.kt
@@ -0,0 +1,7 @@
+package com.texnodevcom.texnodev.model
+
+data class CategoryNews(
+        val id: Int,
+        val name: String,
+        val image: Int
+)
